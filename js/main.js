60f61d4a0d80fd5272f1c610e37e89579f90da21
Manfred Obumma <fredobumma@gmail.com>
2022-04-29 01:45:51 +0100

    Building the FAQ component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 function lockScroll() {
     document.body.classList.toggle("lock-scroll");
 }
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+    item.addEventListener("click", function () {
+        this.classList.toggle("collapsible--expanded");
+    })
+);
